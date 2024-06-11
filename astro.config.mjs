935659d4b085b9f3c224ff65e0300246107bd81e
komiyama029 <komiyama@ekanlab.co.jp>
2024-06-11 17:22:27 +0900

    記事取得部分仮実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  // server: {
+  //   proxy: {
+  //     api: {
+  //       target: import.meta.env.PUBLIC_API_URL,
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, "/wp-json/wp/v2"),
+  //     },
+  //   },
+  // },
+});

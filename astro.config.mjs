edf044cbf5cf8bcaeb944d75e61ce887419e9275
komiyama029 <komiyama@ekanlab.co.jp>
2024-06-19 15:10:34 +0900

    proxyテスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from "astro/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  // server: {
-  //   proxy: {
-  //     api: {
-  //       target: import.meta.env.PUBLIC_API_URL,
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, "/wp-json/wp/v2"),
-  //     },
-  //   },
-  // },
+  vite: {
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.PUBLIC_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/wp-json/wp/v2"),
+        },
+      },
+    },
+  },
 });
